53dbad51937ac67b29c111224f910eec967e336b
Ruchika Runwal <ruchikarunwal15@gmail.com>
2024-09-21 20:08:19 +0530

    Did tailwind setup to create responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1>Hello, Welcome to the app</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello, Welcome to the app
+      </h1>
     </>
   );
 }
